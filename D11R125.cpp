58ff323b3dd36124e8e9bc4fb4118dfe9b040335
Ashutosh Srivastava <95545385+XERXES-OG@users.noreply.github.com>
2023-08-01 02:20:33 +0530

    Add files via upload
    Day 11

---
+++ D11R125.cpp
@@ -0,0 +1,47 @@
+class MyQueue {
+public:
+    stack<int> st;
+    stack<int> s;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+    if(st.empty())
+    {
+        st.push(x);
+        return;
+    }        
+    int temp=st.top();
+    st.pop();
+    push(x);
+    st.push(temp);
+    }
+    
+    int pop() {
+        
+        int a=st.top();
+        st.pop();
+        return a;
+        
+    }
+    
+    int peek() {
+        return st.top();
+    }
+    
+    bool empty() {
+        return st.empty();
+
+    }
+   
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
